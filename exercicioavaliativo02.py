7a591284d92bdedbc5fed5af2048f23da6072e82
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 20:49:07 -0300

    Add files via upload

---
+++ exercicioavaliativo02.py
@@ -0,0 +1,7 @@
+# Peça ao usuario dois numeros e mostre a subtração deles
+n1 = int(input("Digite o primeiro número:\n"))
+n2 = int(input("Digite o segundo número:\n"))
+
+subtracao = ( n1 - n2)
+
+print(" A subtração de dois números é:\n", subtracao)
